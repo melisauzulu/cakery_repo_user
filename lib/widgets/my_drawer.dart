a9890e85c9a700291925f8fbeb01c2baaf375486
Melisa <melisauzulu@yahoo.com>
2023-02-08 11:09:20 -0800

    drawer menü eklendi

--- lib/widgets/my_drawer.dart
+++ lib/widgets/my_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:cakery_app_users_app/authentication/auth_screen.dart';
 import 'package:cakery_app_users_app/global/global.dart';
 import 'package:flutter/material.dart';
 
@@ -40,7 +41,143 @@ class MyDrawer extends StatelessWidget {
             ),
           ),
 
+          const SizedBox(height:12),
+
           //Body Drawer
+
+          Container(
+
+            padding: const EdgeInsets.only(top:1.0),
+            child:Column(
+              children:[
+                const Divider(
+                   height:10,
+                   color: Colors.pink,
+                   thickness: 2,),
+                   ListTile(
+                    leading:  Icon(Icons.home,color:Colors.pink[500]),
+                    title: const Text(
+                       "Home",
+                       style:TextStyle(color:Colors.black,),
+
+                       ),
+
+                       onTap:(() {
+                         //For Later
+                       }) ,
+                       
+                      
+                   ),
+                   const Divider(
+                   height:10,
+                   color: Colors.pink,
+                   thickness: 2,),
+                   ListTile(
+                    leading:  Icon(Icons.reorder,color:Colors.pink[500]),
+                    title: const Text(
+                       "My Orders",
+                       style:TextStyle(color:Colors.black,),
+
+                       ),
+
+                       onTap:(() {
+                         //For Later
+                       }) ,               
+
+                   ),
+
+                   const Divider(
+                   height:10,
+                   color: Colors.pink,
+                   thickness: 2,),
+
+                    ListTile(
+                    leading:  Icon(Icons.access_time,color:Colors.pink[500]),
+                    title: const Text(
+                       "History",
+                       style:TextStyle(color:Colors.black,),
+
+                       ),
+
+                       onTap:(() {
+                         //For Later
+                       }) ,               
+
+                   ),
+                   const Divider(
+                   height:10,
+                   color: Colors.pink,
+                   thickness: 2,),
+
+                    ListTile(
+                    leading:  Icon(Icons.search,color:Colors.pink[500]),
+                    title: const Text(
+                       "Search",
+                       style:TextStyle(color:Colors.black,),
+
+                       ),
+
+                       onTap:(() {
+                         //For Later
+                       }) ,               
+
+                   ),
+
+                   const Divider(
+                   height:10,
+                   color: Colors.pink,
+                   thickness: 2,),
+
+                    ListTile(
+                    leading:  Icon(Icons.add_location,color:Colors.pink[500]),
+                    title: const Text(
+                       "Add New Address",
+                       style:TextStyle(color:Colors.black,),
+
+                       ),
+
+                       onTap:(() {
+                         //For Later
+                       }) ,               
+
+                   ),
+
+                   const Divider(
+                   height:10,
+                   color: Colors.pink,
+                   thickness: 2,),
+
+                    ListTile(
+                    leading:  Icon(Icons.exit_to_app,color:Colors.pink[500]),
+                    title: const Text(
+                       "Sign Out",
+                       style:TextStyle(color:Colors.black,),
+
+                       ),
+
+                       onTap:() {
+
+                          firebaseAuth.signOut().then((value) { //allow user to logout
+
+                          Navigator.push(context, MaterialPageRoute(builder: (c) => const AuthScreen()));
+
+              }); 
+                       },
+                       ) ,               
+
+                  
+                   
+                   const Divider(
+                   height:10,
+                   color: Colors.pink,
+                   thickness: 2,),
+              
+              
+              ],
+            ),
+
+
+          ),
         ],
       ),
     );
