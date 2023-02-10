0b7546cd1ee2074ed9fe2c9113b95cb22425d00e
Melisa <melisauzulu@yahoo.com>
2023-02-10 02:56:14 -0800

    firestoredan seller infosunu almak için sellera model class oluşturuldu

---
+++ lib/models/sellers.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+class Sellers{
+
+String? sellerName;
+String? sellerUID;
+String?  sellerAvatarUrl;
+String? sellerEmail;
+
+Sellers({ //Constructor
+
+this.sellerName,
+this.sellerUID,
+this.sellerAvatarUrl,
+this.sellerEmail,
+
+});
+
+//Retrieve information from firestore in form of json
+Sellers.fromJson(Map<String, dynamic> json){
+
+sellerName= json["sellerName"];
+sellerUID= json["sellerUID"];
+sellerAvatarUrl= json["sellerAvatarUrl"];
+sellerEmail= json["sellerEmail"];
+}
+
+Map <String,dynamic>toJson(){
+
+final Map<String,dynamic> data=new Map<String,dynamic>();
+
+data["sellerName"]=this.sellerName;
+data["sellerUID"]=this.sellerUID;
+data["sellerAvatarUrl"]=this.sellerAvatarUrl;
+data["sellerEmail"]=this.sellerEmail;
+
+return data;
+
+}
+}
