ea0a9d78dfc38c824d701d35c7448af62c509fd1
Melisa <melisauzulu@yahoo.com>
2023-02-26 07:23:26 -0800

    cakery title haline getirildi

--- lib/widgets/simple_app_bar.dart
+++ lib/widgets/simple_app_bar.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 class SimpleAppBar extends StatelessWidget with PreferredSizeWidget{
 
+  String? title;
   final PreferredSizeWidget? bottom;
 
-  SimpleAppBar({this.bottom});
+  SimpleAppBar({this.bottom,this.title});
 
   @override
   Size get preferredSize => bottom==null?Size(56, AppBar().preferredSize.height):Size(56, 80+AppBar().preferredSize.height);
@@ -33,11 +34,11 @@ class SimpleAppBar extends StatelessWidget with PreferredSizeWidget{
       ) ,
       centerTitle: true,
 
-      title:const Text(
+      title: Text(
 
         // sellerın ismini top bar kısımına yazdırıyoruz
-        "Cakery",
-        style: TextStyle(fontSize:40, letterSpacing: 3,color: Colors.white ,fontFamily: "Signatra"),
+        title!,
+        style: const TextStyle(fontSize:40, letterSpacing: 3,color: Colors.white ,fontFamily: "Signatra"),
       ),
     );
   }
